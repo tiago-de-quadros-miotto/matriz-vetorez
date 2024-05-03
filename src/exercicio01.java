85d06e3cedb30a06b94aff12c93438bcc6dd0c9a
Tiago de Quadros <tiagomiotto56@gmail.com>
2024-05-02 21:56:02 -0300

    matriz-veetorez

---
+++ src/exercicio01.java
@@ -0,0 +1,27 @@
+public class exercicio01 {
+
+    public static void main(String[] args) {
+        
+//Gerar e escrever uma matriz bidimensional de tamanho 9x7, 
+//com números aleatórios entre zero e cem. 
+//Ao final, imprimir a média geral dos números da tabela
+        
+
+   int linhas = 9;
+   int colunas = 7;
+   
+   int[][] = new int[i][j]
+    
+   for (int i = 0; i < args.length; i++) {
+        
+    } 
+       
+
+
+
+
+
+
+    }
+    
+}
